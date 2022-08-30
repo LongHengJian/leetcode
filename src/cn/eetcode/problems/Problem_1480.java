af98d1f2c7717d868a98da907cae3b7d2449875b
LHJ <1162528729@qq.com>
2022-08-30 11:54:56 +0800

    init：初始化

---
+++ src/cn/eetcode/problems/Problem_1480.java
@@ -0,0 +1,11 @@
+package cn.eetcode.problems;
+
+/**
+ * @author 龙恒建
+ * @Email longhengjian@utry.cn
+ * @date 2022-08-30 11:44
+ * @ClassName Problem_1480
+ * @description: 一维数组的动态和
+ */
+public class Problem_1480 {
+}
